71b399cd4da1f686029d086b8fb4d0d028a59832
Andy Rosenwald <roseaj14@wfu.edu>
2016-06-14 17:11:07 -0400

    Update random.js

--- met_chrome_extension_vpattern/random.js
+++ met_chrome_extension_vpattern/random.js
@@ -1,5 +1,5 @@
 <!-- hide script from old browsers
-var howMany = 5;  // max number of items listed below
+var howMany = 6;  // max number of items listed below
 var page = new Array(howMany+1);
 
 page[0]="0001.html";
@@ -8,6 +8,7 @@ page[2]="0003.html";
 page[3]="0004.html";
 page[4]="0005.html";
 page[5]="0006.html";
+page[6]="0007.html";
 
 
 function rndnumber(){
